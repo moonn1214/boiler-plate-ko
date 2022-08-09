cb9db9c490f2304ee2aa948c00d7dcc5d90bd6c6
moonn1214 <moonn1214@github.com>
2022-08-09 17:50:23 +0900

    #13 전까지

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
@@ -10,7 +11,7 @@ const config = require('./config/key');
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 
@@ -30,6 +31,33 @@ app.post('/register', (req, res) => {
         if(err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    // 요청된 이메일을 DB에 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        // 요청된 이메일이 DB에 있다면 비밀번호 일치 여부를 확인한다.
+        user.comparePassword(req.body.password,  (err, isMatch) => {
+            if(!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+            // 비밀번호가 일치하면 토큰을 생성한다.
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+
+                // 토큰을 저장한다. (위치는 쿠키, 로컬스토리지 등 여러 방법 존재)
+                res.cookie("x-auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
+            })      
         })
     })
 })
