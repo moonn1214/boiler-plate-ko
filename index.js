58517bbbd1f331904d54f54710a497caadea01d9
JONG <moonn1214@github.com>
2022-08-09 23:00:17 +0900

    auth, logout 테스트해야 함

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 const cookieParser = require('cookie-parser');
 
@@ -23,7 +24,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원가입 할때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
     const user = new User(req.body)
 
@@ -35,7 +36,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일을 DB에 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if(!user) {
@@ -58,6 +59,30 @@ app.post('/login', (req, res) => {
                     .status(200)
                     .json({ loginSuccess: true, userId: user._id })
             })      
+        })
+    })
+})
+
+app.get('/api/users/auth', auth, (req, res)=> {
+    // 여기까지 미들웨어를 통과한 것은 Authentication이 True라는 의미
+    
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+        if(err) return res.json({ success: false, err });
+        return res.status(200).send({
+            success: true
         })
     })
 })
